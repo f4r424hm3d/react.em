e9168d06af599793b737276e3ae106c632bb01cc
codewithprince01 <codewithprince0@gmail.com>
2026-02-14 11:25:28 +0530

    Fix popup form

--- src/pages/OurPartners/ContactFormPopup.jsx
+++ src/pages/OurPartners/ContactFormPopup.jsx
@@ -277,7 +277,7 @@ const ContactFormPopup = ({ isOpen, onClose }) => {
           {/* Close button */}
           <button
             onClick={onClose}
-            className="absolute top-4 right-4 text-gray-500 hover:text-gray-700"
+            className="absolute top-2 right-2 text-blue-100 hover:text-white cursor-pointer"
           >
             <X className="w-6 h-6" />
           </button>
@@ -528,7 +528,7 @@ const ContactFormPopup = ({ isOpen, onClose }) => {
                 </button>
 
                 {/* Note */}
-                <p className="text-center text-sm text-gray-600">
+                <p className="text-center text-[12px] text-gray-600">
                   ( SUBMIT ONCE – NO MORE POPUPS AFTER THAT! )
                 </p>
               </form>
